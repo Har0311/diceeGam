9838335dd452236a8081980c4b4e9f8f0fa0d9dc
Har0311 <109755673+Har0311@users.noreply.github.com>
2022-09-29 13:02:48 +0530

    newwwdicegame

--- index.js
+++ index.js
@@ -13,22 +13,22 @@ function changeColor1(){
 
 
     if(rndIntegers1===1 ){
-        document.querySelector(".spic1").src = "images/dice-1.png";
+        document.querySelector(".spic1").src = "dice-1.png";
     }
     else if (rndIntegers1===2){
-        document.querySelector(".spic1").src = "images/dice-2.png";
+        document.querySelector(".spic1").src = "dice-2.png";
     }
     else if(rndIntegers1===3){
-        document.querySelector(".spic1").src = "images/dice-3.png";
+        document.querySelector(".spic1").src = "dice-3.png";
     }
     else if(rndIntegers1===4){
-        document.querySelector(".spic1").src = "images/dice-4.png";
+        document.querySelector(".spic1").src = "dice-4.png";
     }
     else if(rndIntegers1===5){
-        document.querySelector(".spic1").src = "images/dice-5.png";
+        document.querySelector(".spic1").src = "dice-5.png";
     }
     else if(rndIntegers1===6){
-        document.querySelector(".spic1").src = "images/dice-6.png";
+        document.querySelector(".spic1").src = "dice-6.png";
     }
 
     
@@ -37,22 +37,22 @@ function changeColor1(){
 function changeColor2(){
 
     if(rndIntegers2===1){
-        document.querySelector(".spic2").src = "images/dice-1.png";
+        document.querySelector(".spic2").src = "dice-1.png";
     }
     else if (rndIntegers2===2){
-        document.querySelector(".spic2").src = "images/dice-2.png";
+        document.querySelector(".spic2").src = "dice-2.png";
     }
     else if(rndIntegers2===3){
-        document.querySelector(".spic2").src = "images/dice-3.png";
+        document.querySelector(".spic2").src = "dice-3.png";
     }
     else if(rndIntegers2===4){
-        document.querySelector(".spic2").src = "images/dice-4.png";
+        document.querySelector(".spic2").src = "dice-4.png";
     }
     else if(rndIntegers2===5){
-        document.querySelector(".spic2").src = "images/dice-5.png";
+        document.querySelector(".spic2").src = "dice-5.png";
     }
     else if(rndIntegers2===6){
-        document.querySelector(".spic2").src = "images/dice-6.png";
+        document.querySelector(".spic2").src = "dice-6.png";
     }
 
 
